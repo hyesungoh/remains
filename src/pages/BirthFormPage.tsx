d9a1eb44c90dde70d7b2b0d1b62f286be65e661e
hyesungoh <haesungoh414@gmail.com>
2025-01-27 01:54:48 +0900

    birth date with select

--- src/pages/BirthFormPage.tsx
+++ src/pages/BirthFormPage.tsx
@@ -1,28 +1,35 @@
-
-import { useStorage } from "@plasmohq/storage/hook";
-
-
 import { useState } from "react";
 import BlurText from "~components/BlureText";
 import { Button } from "~components/Button";
-import { Dialog, DialogContent, DialogTitle, DialogDescription, DialogFooter, DialogHeader, DialogTrigger } from "~components/Dialog";
-import { Input } from "~components/Input";
+import { Dialog, DialogContent, DialogTitle, DialogDescription, DialogFooter, DialogTrigger } from "~components/Dialog";
+import { Select, SelectItem, SelectTrigger, SelectContent, SelectValue, SelectGroup } from "~components/Select";
 import { useBirth } from "~hooks/useBirth";
 
 export default function BirthFormPage() {
   const [, setBirth] = useBirth();
 
-  const [innerBirth, setInnerBirth] = useState('');
-  const onChangeBirth = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const birth = e.target.value;
-    setInnerBirth(birth);
-  }
+  const [year, setYear] = useState('');
+  const [month, setMonth] = useState('');
+  const [day, setDay] = useState('');
 
-  const isAvailableBirth = innerBirth.length >= 6;
-  
+  const handleYearChange = (value: string) => {
+    setYear(value);
+  };
+  const handleMonthChange = (value: string) => {
+    setMonth(value);
+  };
+  const handleDayChange = (value: string) => {
+    setDay(value);
+  };
+
+  const isAvailableBirth = (() => {
+    if (year === '' || month === '' || day === '') return false;
+    return true;
+  })()
+
   const onClickSubmit = () => {
     if (!isAvailableBirth) return;
-    setBirth(innerBirth);
+    setBirth(`${year}-${month}-${day}`);
   }
 
   return <div className="flex flex-col items-center justify-center h-full">
@@ -35,11 +42,61 @@ export default function BirthFormPage() {
     />
 
     <div className="flex gap-4">
-      <Input type='date' className='w-40' onChange={onChangeBirth} value={innerBirth} />
+      <div className="flex gap-1">
+        <Select value={year} onValueChange={handleYearChange}>
+          <SelectTrigger>
+            <SelectValue placeholder="year" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              {Array.from({ length: 2025 - 1900 + 1 }, (_, i) => 2025 - i).map(
+                (year) => (
+                  <SelectItem key={year} value={String(year)}>
+                    {year}
+                  </SelectItem>
+                )
+              )}
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+
+        <Select value={month} onValueChange={handleMonthChange}>
+          <SelectTrigger>
+            <SelectValue placeholder="month" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="01">1</SelectItem>
+            <SelectItem value="02">2</SelectItem>
+            <SelectItem value="03">3</SelectItem>
+            <SelectItem value="04">4</SelectItem>
+            <SelectItem value="05">5</SelectItem>
+            <SelectItem value="06">6</SelectItem>
+            <SelectItem value="07">7</SelectItem>
+            <SelectItem value="08">8</SelectItem>
+            <SelectItem value="09">9</SelectItem>
+            <SelectItem value="10">10</SelectItem>
+            <SelectItem value="11">11</SelectItem>
+            <SelectItem value="12">12</SelectItem>
+          </SelectContent>
+        </Select>
+
+        <Select value={day} onValueChange={handleDayChange}>
+          <SelectTrigger>
+            <SelectValue placeholder="day" />
+          </SelectTrigger>
+          <SelectContent>
+            {Array.from({ length: 31 }, (_, i) => i + 1).map(
+              (day) => (
+                <SelectItem key={day} value={String(day)}>{day}</SelectItem>
+              )
+            )}
+          </SelectContent>
+        </Select>
+      </div>
 
       <Dialog>
         <DialogTrigger>
-          <Button variant='secondary' disabled={!isAvailableBirth}>
+          <Button disabled={!isAvailableBirth}>
             Submit
           </Button>
         </DialogTrigger>
@@ -47,7 +104,7 @@ export default function BirthFormPage() {
         <DialogContent>
           <DialogTitle>Confirm your birthdate</DialogTitle>
           <DialogDescription>
-            {innerBirth}
+            {`${year}-${month}-${day}`}
           </DialogDescription>
           <DialogFooter>
             <Button onClick={onClickSubmit}>Submit</Button>
